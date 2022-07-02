aa34b8e9b837af5bb265fe4170e9fb04a88ef2ac
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-02 06:58:44 +0000

    [Finish]Rails6-bookers2-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    #@book = Book.find(params[:book_id])
-    #@book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = Book.find(params[:book_id]).id
     comment.save
-    redirect_to book_path(comment.book_id)
   end
 
   def destroy
-    #@book = Book.find(params[:book_id])
-    #@book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
